24d4248db9248e739c71a7f94334b397341981b3
gianguno10508 <gianguno10508@gmail.com>
2022-03-30 09:46:21 +0700

    Test update

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Test
+          This is test
         </p>
         <a
           className="App-link"

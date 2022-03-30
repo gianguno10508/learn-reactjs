2cdb3ff685e6e504e3bda93530f1c8d513a88b92
gianguno10508 <gianguno10508@gmail.com>
2022-03-30 10:28:08 +0700

    Test update 3

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Con mẹ thằng Tú
+          Lôn Anh Tú
         </p>
         <a
           className="App-link"
